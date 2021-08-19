78c26f6eb34eb8b8bc325de94b09aff082448d48
Justin Grant <justin@Justins-Air.home>
2021-08-18 19:48:20 -0700

    connected server

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require('express')
+var mysql = require('mysql');
+const app = express()
 
-let port = process.env.PORT || 3000;
+require("dotenv").config()
 
-const app = express();
+const host = process.env.HOST
+const user = process.env.MYUSER
+const password = process.env.PASSWORD
+const database = process.env.DATABASE
+console.log(database)
 
-app.get('/', (req, res) => {
-    res.send('hello world?????');
+const db = mysql.createConnection({
+  host: host,
+  user: user,
+  password: password,
+  database: database
 });
 
-app.listen(port, () => {
-    console.log(`Example app is running on port ${port}`);
-});
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("MySql Connected...")
+    })
+
+    
+app.get('/', (req,res) => {
+    const sql = "SELECT * FROM Songs";
+    db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result)
+    })
+})
+
+app.listen('3000', () => {
+    console.log("Server started on port 3000")
+})
