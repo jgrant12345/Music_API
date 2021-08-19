417ca65baeece20d3fb67d311cbb2ceb211a6572
Justin Grant <justin@Justins-Air.home>
2021-08-18 20:58:31 -0700

    added

--- app.js
+++ app.js
@@ -3,29 +3,34 @@ var mysql = require('mysql');
 const app = express()
 var PORT = process.env.PORT || 3000
 
-// require("dotenv").config()
+require("dotenv").config()
 
 const host = process.env.HOST
-// const user = process.env.MYUSER
-// const password = process.env.PASSWORD
-// const database = process.env.DATABASE
+const user = process.env.MYUSER
+const password = process.env.PASSWORD
+const database = process.env.DATABASE
 
 
-// const db = mysql.createConnection({
-//   host: host,
-//   user: user,
-//   password: password,
-//   database: database
-// });
+const db = mysql.createConnection({
+  host: host,
+  user: user,
+  password: password,
+  database: database
+});
 
-// db.connect(function(err) {
-//     if (err) throw err;
-//     console.log("MySql Connected...")
-//     })
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("MySql Connected...")
+    })
 
-app.get('/', (req,res) => {
-   res.send(host)
-})
+    app.get('/', (req,res) => {
+      const sql = "SELECT * FROM Songs";
+      db.query(sql, (err,result) => {
+          if(err) throw err;
+          console.log(result);
+          res.send(result)
+      })
+  })
     
 
 
